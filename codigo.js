8c466abd5b7c816320e003bd55c5a757706375e3
Carolina Martinez <carolinamtz091@gmail.com>
2024-11-20 23:28:55 -0600

    creando login con php

---
+++ codigo.js
@@ -0,0 +1,45 @@
+$('#formLogin').submit(function(e){
+    e.preventDefault();
+    var usuario = $.trim($("#usuario").val());
+    var password = $.trim($("#password").val());
+
+    if(usuario.length == "" || password == ""){
+        Swal.fire({
+            type:'warning',
+            title:'Debe ingresar un usuario y/o password',
+        });
+        return false;
+    } else {
+        $.ajax({
+            url:"bd/login.php",
+            type: "POST",
+            datatype: "json",
+            data: {usuario:usuario, password:password},
+            success: function(data){
+                if(data == "null"){
+                    Swal.fire({
+                        type:'error',
+                        title: 'usuario y/o password incorrecta',
+
+                    });
+
+                }else {
+                    Swal.fire({
+                        type:'success',
+                        title:'Conexión exitosa!',
+                        confirmButtonColor: '#3985d6',
+                        confirmButtonText: 'Ingresar'
+                    }).then((result) => {
+                        if(result.value){
+                            window.location.href = "vistas/pag_incio.php";
+                        }
+                    })
+
+                }
+            }
+
+        });
+    }
+
+
+});
